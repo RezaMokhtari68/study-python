46ec14cd07cbb36430bbc455d15d034b0c9ee044
reza.mokhtari <reza.mokhtari68@gmail.com>
2018-02-12 17:26:33 +0330

    better code style

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
     # Type your code here
 a = input('Please type a number between 1 to 7: ')
 
-x = int(a)-1
+x = int(a) - 1
 
 y = ['MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY']
 
